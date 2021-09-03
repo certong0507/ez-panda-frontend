9570598a29ed64ddf64f3015d2b63abad90112f8
certong <tongtong0507@live.com>
2021-09-03 19:47:02 +0800

    + Login and logout process changes

--- src/app/modules/auth/sign-out/sign-out.component.ts
+++ src/app/modules/auth/sign-out/sign-out.component.ts
@@ -5,28 +5,22 @@ import { finalize, takeUntil, takeWhile, tap } from 'rxjs/operators';
 import { AuthService } from 'app/core/auth/auth.service';
 
 @Component({
-    selector     : 'auth-sign-out',
-    templateUrl  : './sign-out.component.html',
-    encapsulation: ViewEncapsulation.None
+    selector: 'auth-sign-out',
+    templateUrl: './sign-out.component.html',
+    encapsulation: ViewEncapsulation.None,
 })
-export class AuthSignOutComponent implements OnInit, OnDestroy
-{
+export class AuthSignOutComponent implements OnInit, OnDestroy {
     countdown: number = 5;
     countdownMapping: any = {
-        '=1'   : '# second',
-        'other': '# seconds'
+        '=1': '# second',
+        other: '# seconds',
     };
     private _unsubscribeAll: Subject<any> = new Subject<any>();
 
     /**
      * Constructor
      */
-    constructor(
-        private _authService: AuthService,
-        private _router: Router
-    )
-    {
-    }
+    constructor(private _authService: AuthService, private _router: Router) {}
 
     // -----------------------------------------------------------------------------------------------------
     // @ Lifecycle hooks
@@ -35,29 +29,31 @@ export class AuthSignOutComponent implements OnInit, OnDestroy
     /**
      * On init
      */
-    ngOnInit(): void
-    {
+    ngOnInit(): void {
         // Sign out
-        this._authService.signOut();
+        this._authService.signOut().subscribe((result) => {
+            if (result) {
+                this._router.navigate(['sign-in']);
+            }
+        });
 
         // Redirect after the countdown
-        timer(1000, 1000)
-            .pipe(
-                finalize(() => {
-                    this._router.navigate(['sign-in']);
-                }),
-                takeWhile(() => this.countdown > 0),
-                takeUntil(this._unsubscribeAll),
-                tap(() => this.countdown--)
-            )
-            .subscribe();
+        // timer(1000, 1000)
+        //     .pipe(
+        //         finalize(() => {
+        //             this._router.navigate(['sign-in']);
+        //         }),
+        //         takeWhile(() => this.countdown > 0),
+        //         takeUntil(this._unsubscribeAll),
+        //         tap(() => this.countdown--)
+        //     )
+        //     .subscribe();
     }
 
     /**
      * On destroy
      */
-    ngOnDestroy(): void
-    {
+    ngOnDestroy(): void {
         // Unsubscribe from all subscriptions
         this._unsubscribeAll.next();
         this._unsubscribeAll.complete();
