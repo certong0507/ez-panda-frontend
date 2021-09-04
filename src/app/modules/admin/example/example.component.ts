ff176c432c097a5a561675a5f929502ca53aef32
certong <tongtong0507@live.com>
2021-09-04 13:07:48 +0800

    + Dashboard barchat

--- src/app/modules/admin/example/example.component.ts
+++ src/app/modules/admin/example/example.component.ts
@@ -7,7 +7,17 @@ import {
     ViewChild,
     ViewEncapsulation,
 } from '@angular/core';
-import { ApexOptions } from 'ng-apexcharts';
+import {
+    ApexAxisChartSeries,
+    ApexChart,
+    ChartComponent,
+    ApexDataLabels,
+    ApexPlotOptions,
+    ApexYAxis,
+    ApexTitleSubtitle,
+    ApexXAxis,
+    ApexFill,
+} from 'ng-apexcharts';
 import { MatSort } from '@angular/material/sort';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
@@ -17,16 +27,29 @@ import { FuseConfirmationService } from '@fuse/services/confirmation';
 import { FinanceService } from 'app/modules/admin/example/finance.service';
 import { UserDetailsDialog } from 'app/layout/common/dialogs/user-details-dialog/user-details-dialog.component';
 
+export type ChartOptions = {
+    series: ApexAxisChartSeries;
+    chart: ApexChart;
+    dataLabels: ApexDataLabels;
+    plotOptions: ApexPlotOptions;
+    yaxis: ApexYAxis;
+    xaxis: ApexXAxis;
+    fill: ApexFill;
+    title: ApexTitleSubtitle;
+};
+
 @Component({
     selector: 'example',
     templateUrl: './example.component.html',
     encapsulation: ViewEncapsulation.None,
 })
 export class ExampleComponent {
-    @ViewChild('recentTransactionsTable', { read: MatSort })
+    // @ViewChild('recentTransactionsTable', { read: MatSort })
+    @ViewChild('chart')
+    chart: ChartComponent;
+    public chartOptions: Partial<ChartOptions>;
     recentTransactionsTableMatSort: MatSort;
     data: any;
-    accountBalanceOptions: ApexOptions;
     recentTransactionsDataSource: MatTableDataSource<any> =
         new MatTableDataSource();
     recentTransactionsTableColumns: string[] = [
@@ -97,47 +120,103 @@ export class ExampleComponent {
     }
 
     private _prepareChartData(): void {
-        // Account balance
-        this.accountBalanceOptions = {
+        this.chartOptions = {
+            series: [
+                {
+                    name: 'Inflation',
+                    data: [2.3, 3.1, 4.0],
+                },
+            ],
             chart: {
-                animations: {
-                    speed: 400,
-                    animateGradually: {
-                        enabled: false,
+                toolbar: {
+                    show: false,
+                },
+                height: 350,
+                type: 'bar',
+            },
+            plotOptions: {
+                bar: {
+                    dataLabels: {
+                        position: 'top', // top, center, bottom
                     },
                 },
-                fontFamily: 'inherit',
-                foreColor: 'inherit',
-                width: '100%',
-                height: '100%',
-                type: 'area',
-                sparkline: {
-                    enabled: true,
+            },
+            dataLabels: {
+                enabled: true,
+                formatter: function (val) {
+                    return val + '%';
+                },
+                offsetY: -20,
+                style: {
+                    fontSize: '12px',
+                    colors: ['#304758'],
                 },
             },
-            colors: ['#A3BFFA', '#667EEA'],
-            fill: {
-                colors: ['#CED9FB', '#AECDFD'],
-                opacity: 0.5,
-                type: 'solid',
-            },
-            series: this.data.accountBalance.series,
-            stroke: {
-                curve: 'straight',
-                width: 2,
-            },
-            tooltip: {
-                followCursor: true,
-                theme: 'dark',
-                x: {
-                    format: 'MMM dd, yyyy',
-                },
-                y: {
-                    formatter: (value): string => value + '%',
-                },
-            },
+
             xaxis: {
-                type: 'datetime',
+                categories: ['Jan', 'Feb', 'Mar'],
+                position: 'top',
+                labels: {
+                    offsetY: -18,
+                },
+                axisBorder: {
+                    show: false,
+                },
+                axisTicks: {
+                    show: false,
+                },
+                crosshairs: {
+                    fill: {
+                        type: 'gradient',
+                        gradient: {
+                            colorFrom: '#D8E3F0',
+                            colorTo: '#BED1E6',
+                            stops: [0, 100],
+                            opacityFrom: 0.4,
+                            opacityTo: 0.5,
+                        },
+                    },
+                },
+                tooltip: {
+                    enabled: true,
+                    offsetY: -35,
+                },
+            },
+            // fill: {
+            //     type: 'gradient',
+            //     gradient: {
+            //         shade: 'light',
+            //         type: 'horizontal',
+            //         shadeIntensity: 0.25,
+            //         gradientToColors: undefined,
+            //         inverseColors: true,
+            //         opacityFrom: 1,
+            //         opacityTo: 1,
+            //         stops: [50, 0, 100, 100],
+            //     },
+            // },
+            yaxis: {
+                axisBorder: {
+                    show: false,
+                },
+                axisTicks: {
+                    show: false,
+                },
+                labels: {
+                    show: false,
+                    formatter: function (val) {
+                        return val + '%';
+                    },
+                },
+            },
+            title: {
+                // text: 'Monthly Inflation in Argentina, 2002',
+                // floating: 0,
+                offsetY: 320,
+                align: 'center',
+                style: {
+                    color: '#444',
+                },
             },
         };
     }
@@ -145,7 +224,7 @@ export class ExampleComponent {
     openDialog(): void {
         const dialogRef = this.dialog.open(UserDetailsDialog, {
             maxWidth: '90vw',
-            width: '90%'
+            width: '90%',
         });
 
         dialogRef.afterClosed().subscribe((result) => {
