f8c9da16d6d13ec26f1dae79b02bef56647d5746
ykssheng <ykssheng@gmail.com>
2021-09-04 22:33:05 +0800

    Remove side menu

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -2,34 +2,34 @@
 import { FuseNavigationItem } from '@fuse/components/navigation';
 
 export const defaultNavigation: FuseNavigationItem[] = [
-    {
-        id   : 'example',
-        title: 'Dashboard',
-        type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
-        link : '/example'
-    },
-    {
-        id   : 'manage_wallet',
-        title: 'Manage Users',
-        type : 'basic',
-        icon : 'heroicons_outline:user',
-        link : '/manage-wallet'
-    },
-    {
-        id   : 'manage_award',
-        title: 'Manage Missions',
-        type : 'basic',
-        icon : 'heroicons_outline:gift',
-        link : '/mission'
-    },
-    {
-        id   : 'tranfer',
-        title: 'Transfer',
-        type : 'basic',
-        icon : 'heroicons_outline:switch-horizontal',
-        link : '/transfer'
-    },
+    // {
+    //     id   : 'example',
+    //     title: 'Dashboard',
+    //     type : 'basic',
+    //     icon : 'heroicons_outline:chart-pie',
+    //     link : '/example'
+    // },
+    // {
+    //     id   : 'manage_wallet',
+    //     title: 'Manage Users',
+    //     type : 'basic',
+    //     icon : 'heroicons_outline:user',
+    //     link : '/manage-wallet'
+    // },
+    // {
+    //     id   : 'manage_award',
+    //     title: 'Manage Missions',
+    //     type : 'basic',
+    //     icon : 'heroicons_outline:gift',
+    //     link : '/mission'
+    // },
+    // {
+    //     id   : 'tranfer',
+    //     title: 'Transfer',
+    //     type : 'basic',
+    //     icon : 'heroicons_outline:switch-horizontal',
+    //     link : '/transfer'
+    // },
     {
         id   : 'mission_reward',
         title: 'Redeem',
@@ -37,13 +37,13 @@ export const defaultNavigation: FuseNavigationItem[] = [
         icon : 'heroicons_outline:switch-horizontal',
         link : '/mission-reward'
     },
-    {
-        id   : 'learning_course',
-        title: 'Learning Course',
-        type : 'basic',
-        icon : 'heroicons_outline:book-open',
-        link : '/courses'
-    },
+    // {
+    //     id   : 'learning_course',
+    //     title: 'Learning Course',
+    //     type : 'basic',
+    //     icon : 'heroicons_outline:book-open',
+    //     link : '/courses'
+    // },
     {
         id   : 'sign_out',
         title: 'Sign Out',
